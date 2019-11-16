df357b985c422ed6a4f69fd83978ef67be3d2031
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2019-11-16 21:18:47 +0300

    Выполнил задания 2 и 3

--- src/metrics_server/api/files.clj
+++ src/metrics_server/api/files.clj
@@ -19,3 +19,40 @@
   []
   (:data (get-files-with-http-info)))
 
+;; Задание 2-1
+(defn task21 [files]
+  (filter (fn [a] (get a :directory)) files)
+  )
+
+;; (metrics-server.api.files/task21 (metrics-server.api.files/get-files))
+
+;; Задание 2-2
+(defn task22 [files]
+  (filter (fn [a] (get a :executable)) files)
+  )
+
+;; (metrics-server.api.files/task22 (metrics-server.api.files/get-files))
+
+;; Задание 2-3
+(defn task23 [files]
+  (map (fn [file]
+         {
+          :name (clojure.string/replace (get file :name) #".conf" ".cfg")
+          :size (get file :size)
+          :changed (get file :changed)
+          :directory (get file :directory)
+          :executable (get file :executable)
+          }
+         ) files)
+  )
+
+;; (metrics-server.api.files/task23 (metrics-server.api.files/get-files))
+
+;; Задание 3
+(defn task3 [files]
+  (reduce + (map (fn [file] (get file :size)) (filter (fn [a] (not (get a :directory))) files)))
+  (count (filter (fn [a] (not (get a :directory))) files))
+  )
+
+;; (metrics-server.api.files/task3 (metrics-server.api.files/get-files))
+
