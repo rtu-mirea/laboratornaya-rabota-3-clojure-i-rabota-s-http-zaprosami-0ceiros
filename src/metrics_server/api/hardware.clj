ba369ae76008b6efa11a5a831e95cad8c7c3e1a6
Иван Васильев <52786880+0ceiros@users.noreply.github.com>
2019-11-16 14:11:48 +0300

    Выполнил задание 1

--- src/metrics_server/api/hardware.clj
+++ src/metrics_server/api/hardware.clj
@@ -19,3 +19,9 @@
   []
   (:data (get-metrics-with-http-info)))
 
+;; Задание 1
+(defn task1 [metrics]
+  (filter (fn [a] (< (get a :cpuTemp) 2)) metrics)
+  )
+;; (metrics-server.api.hardware/task1 (metrics-server.api.hardware/get-metrics))
+
